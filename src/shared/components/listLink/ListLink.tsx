b2cc64f334e0966d98d521d1de45a6d231c696be
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-03-05 18:09:04 +0300

    fix

--- src/shared/components/listLink/ListLink.tsx
+++ src/shared/components/listLink/ListLink.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import style from './listlink.module.scss';
+import React from "react";
+import downBtn from "../../assets/icon/down.svg";
+import style from "./listlink.module.scss";
 
 export const ListLink: React.FC = (): React.JSX.Element => {
-  return (
-		<ul className={style.listLink}>
-			<li>Категории</li>
-			<li>Каталог</li>
-			<li>Избранное</li>
-			<li>О компании</li>
-			<li>Вакансии</li>
-		</ul>
-  );
-}
+	const changeDrop = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (window.innerWidth < 768) {
+			const drop: string =
+				e.currentTarget.dataset.drop === "false" ? "true" : "false";
+			e.currentTarget.setAttribute("data-drop", drop);
+		}
+	};
+
+	return (
+		<div className={style.listLink}>
+			<div className={style.title} data-drop={window.innerWidth < 768 ? false: true} onClick={changeDrop}>
+				<h3>Название блока</h3>
+				<img
+					src={downBtn}
+					style={{
+						display: `${
+							window.innerWidth < 768 ? "inline-block" : "none"
+						}`,
+					}}
+					alt="icon-down"
+				/>
+			</div>
+
+			<ul>
+				<li>Категории</li>
+				<li>Каталог</li>
+				<li>Избранное</li>
+				<li>О компании</li>
+				<li>Вакансии</li>
+			</ul>
+		</div>
+	);
+};
